a74291c8553beb67aee5b2aca8974e9654c4dc99
kick_sim <tlacksgml97@gmail.com>
2024-11-20 17:30:49 +0900

    initial commit

---
+++ src/main/java/com/example/yeah/YeahApplication.java
@@ -0,0 +1,13 @@
+package com.example.yeah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yeah/YeahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yeah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
