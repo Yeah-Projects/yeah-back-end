8e362ca9e6c672d286a03e41885dc52cfbb37319
kick_sim <tlacksgml97@gmail.com>
2024-11-22 18:52:32 +0900

    add swagger

--- src/main/java/com/example/yeah/guestbook/controller/GuestBookController.java
+++ src/main/java/com/example/yeah/guestbook/controller/GuestBookController.java
@@ -12,13 +12,18 @@ import com.example.yeah.base.api.ApiResponse;
 import com.example.yeah.guestbook.dto.GuestBookDto;
 import com.example.yeah.guestbook.service.GuestBookService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "guestboom api", description = "방명록에 관한 api")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/guestbook")
 public class GuestBookController {
 	private final GuestBookService guestBookService;
+
+	@Operation(summary = "방명록 작성", description = "방명록을 작성합니다")
 	@PostMapping("/write")
 	public ResponseEntity<ApiResponse<Void>> enroll(
 		@RequestBody GuestBookDto.GuestBookWriteRequest request) {
@@ -27,6 +32,7 @@ public class GuestBookController {
 			.body(ApiResponse.createSuccessWithNoContent());
 	}
 
+	@Operation(summary = "방명록 불러오기", description = "최신 방명록 50개를 가져옵니다")
 	@GetMapping("/get")
 	public ResponseEntity<ApiResponse<GuestBookDto.GuestBookListResponse>> getGuestBookList(){
 		GuestBookDto.GuestBookListResponse response = guestBookService.getList();
